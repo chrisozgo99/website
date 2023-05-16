b98bc19d344b8f940cc313682f298723a7362c0d
Chris Ozgo <chrisozgo99@gmail.com>
2023-05-16 09:46:21 -0400

    fix: updated home meta tags

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -31,8 +31,8 @@ const Index = () => {
     <Main
       meta={
         <Meta
-          title="Next.js Boilerplate Presentation"
-          description="Next js Boilerplate is the perfect starter code for your project. Build your React application with the Next.js framework."
+          title="Chris Ozgo"
+          description="The personal website and blog of Chris Ozgo"
         />
       }
     >
