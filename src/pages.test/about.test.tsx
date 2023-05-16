1f69e532d87b11b5b0c3c79611e87b51ab37e0c2
Chris Ozgo <chrisozgo99@gmail.com>
2023-05-16 14:28:44 -0400

    fix: tests

--- src/pages.test/about.test.tsx
+++ src/pages.test/about.test.tsx
@@ -1,20 +1,18 @@
-/* eslint-disable jest/no-commented-out-tests */
-// import { render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
-// import About from '@/pages/about';
+import About from '@/pages/about';
 
-// // The easiest solution to mock `next/router`: https://github.com/vercel/next.js/issues/7479
-// // The mock has been moved to `__mocks__` folder to avoid duplication
+// The easiest solution to mock `next/router`: https://github.com/vercel/next.js/issues/7479
+// The mock has been moved to `__mocks__` folder to avoid duplication
 
-// describe('About page', () => {
-//   describe('Render method', () => {
-//     it('should have two paragraphs of `Lorem ipsum`', () => {
-//       render(<About />);
+describe('About page', () => {
+  describe('Render method', () => {
+    it('should say "About Me"`', () => {
+      render(<About />);
 
-//       const paragraph = screen.getAllByText(/Lorem ipsum/);
+      const paragraph = screen.getByText(/About Me/);
 
-//       expect(paragraph).toHaveLength(2);
-//     });
-//   });
-// });
-export {};
+      expect(paragraph).toBeInTheDocument();
+    });
+  });
+});
