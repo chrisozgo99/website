454bed36ce06391f02cb2d406caa334c4cd21b32
Chris Ozgo <chrisozgo99@gmail.com>
2023-09-19 11:16:30 +0200

    feat: added beginning of search component

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -2,21 +2,36 @@ import React from 'react';
 
 const Search = () => {
   return (
-    <section id="q-search-wrapper" className="">
-      <div className="">
-        <header className="">
+    <section id="" className="mr-4">
+      <div className="rounded-md border border-gray-300">
+        <header className="items-center">
           <span className="" />
           <input
-            id="q-search-input"
             name="search"
-            className=""
+            className="my-1 mr-2 w-fit px-6 py-2 text-sm outline-none"
             type="text"
-            placeholder='Press "ctrl + k" to search'
-            aria-label="Search"
-            aria-describedby="q-search-list"
+            placeholder="Search"
           />
-          <button id="q-search-delete" type="button" className="">
-            Search
+          <button type="button" className="px-6 py-2 text-sm">
+            <svg
+              className="h-4 w-4 fill-current text-gray-500"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              <circle
+                className="opacity-25"
+                cx="11"
+                cy="11"
+                r="8"
+                stroke="currentColor"
+                strokeWidth="2"
+              />
+              <path
+                className="opacity-75"
+                fill="currentColor"
+                d="M21 21l-4.35-4.35"
+              />
+            </svg>
           </button>
         </header>
       </div>
