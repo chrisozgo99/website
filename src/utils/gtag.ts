3379bcce44704d8a3151ca4104ccb4c4373df0f7
Chris Ozgo <chrisozgo99@gmail.com>
2023-10-15 00:45:07 +0200

    fix: small issues

--- src/utils/gtag.ts
+++ src/utils/gtag.ts
@@ -1,10 +1,9 @@
-export const pageview = (url: string) => {
+export const pageview = (url: any) => {
   window.gtag('config', process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID, {
     page_path: url,
   });
 };
 
-// https://developers.google.com/analytics/devguides/collection/gtagjs/events
 export const event = ({ action, category, label, value }: any) => {
   window.gtag('event', action, {
     event_category: category,
