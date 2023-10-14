e0130a28cba1492767ef4d8214a3e6aa0753a8a7
Chris Ozgo <chrisozgo99@gmail.com>
2023-10-14 21:45:26 +0200

    feat: added search on blog

--- src/pages/api/posts.ts
+++ src/pages/api/posts.ts
@@ -2,12 +2,21 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 
 import { getPosts } from '@/lib/ghost-client';
 
+const fs = require('fs');
+
 export default function getJsonPosts(
   _: NextApiRequest,
   response: NextApiResponse
 ) {
   getPosts()
     .then((posts: any) => {
+      const json = { posts: [] };
+      posts.forEach((post: any) => {
+        json.posts.push(post as never);
+      });
+
+      fs.writeFileSync('public/posts.json', JSON.stringify(json, null, 2));
+
       response.status(200).json(posts);
     })
     .catch((error: any) => {
--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,14 +1,21 @@
 /* eslint-disable react/no-unescaped-entities */
-
+import type { MultipleQueriesQuery } from '@algolia/client-search';
+import algoliasearch from 'algoliasearch/lite';
 import type { GetServerSideProps } from 'next';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { useState } from 'react';
 import InfiniteScroll from 'react-infinite-scroll-component';
+import type { SearchBoxProps } from 'react-instantsearch';
+import {
+  Hits,
+  InstantSearch,
+  SearchBox,
+  useInstantSearch,
+} from 'react-instantsearch';
 
 import BlogPreview from '@/components/blog-preview';
-import Search from '@/components/search';
 import Subscribe from '@/components/subscribe';
 import SubscribeModal from '@/components/subscribe-modal';
 import { Meta } from '@/layouts/Meta';
@@ -47,7 +54,127 @@ interface BlogProps {
   test?: boolean;
 }
 
+function Hit({ hit }: any) {
+  return (
+    <div className="col-span-1 mb-8 w-fit sm:mx-7">
+      <Link
+        className="w-fit"
+        href={{
+          pathname: `/blog/${hit.primary_tag.slug}/${hit.slug}`,
+        }}
+      >
+        <BlogPreview
+          excerpt={hit.excerpt}
+          feature_image={hit.feature_image}
+          feature_image_alt={hit.feature_image_alt}
+          primary_author={hit.primary_author}
+          primary_tag={hit.primary_tag}
+          published_at={hit.published_at}
+          reading_time={hit.reading_time}
+          title={hit.title}
+        />
+      </Link>
+    </div>
+  );
+}
+
+function EmptyQueryBoundary({ children, postList }: any) {
+  const { indexUiState } = useInstantSearch();
+
+  const results = postList.map((post: any, index: number) => {
+    if (index % 2 === 0) {
+      return (
+        <div
+          className="-z-20 sm:flex sm:flex-row"
+          key={post.id + new Date().toLocaleDateString()}
+        >
+          <div className="col-span-1 mb-8 w-fit sm:mx-7" key={post.id}>
+            <Link
+              className="w-fit"
+              href={{
+                pathname: `/blog/${post.primary_tag.slug}/${post.slug}`,
+              }}
+            >
+              <BlogPreview
+                excerpt={post.excerpt}
+                feature_image={post.feature_image}
+                feature_image_alt={post.feature_image_alt}
+                primary_author={post.primary_author}
+                primary_tag={post.primary_tag}
+                published_at={post.published_at}
+                reading_time={post.reading_time}
+                title={post.title}
+              />
+            </Link>
+          </div>
+          {postList[index + 1] && (
+            <div
+              className="col-span-2 mb-8 w-fit sm:mx-7"
+              key={postList[index + 1].id}
+            >
+              <Link
+                className="w-fit"
+                href={{
+                  pathname: `/blog/${postList[index + 1].primary_tag.slug}/${
+                    postList[index + 1].slug
+                  }`,
+                }}
+              >
+                <BlogPreview
+                  excerpt={postList[index + 1].excerpt}
+                  feature_image={postList[index + 1].feature_image}
+                  feature_image_alt={postList[index + 1].feature_image_alt}
+                  primary_author={postList[index + 1].primary_author}
+                  primary_tag={postList[index + 1].primary_tag}
+                  published_at={postList[index + 1].published_at}
+                  reading_time={postList[index + 1].reading_time}
+                  title={postList[index + 1].title}
+                />
+              </Link>
+            </div>
+          )}
+        </div>
+      );
+    }
+    return children;
+  });
+
+  if (!indexUiState.query) {
+    return results;
+  }
+
+  return children;
+}
+
 const Blog = (props: BlogProps) => {
+  const algoliaClient = algoliasearch(
+    process.env.NEXT_PUBLIC_ALGOLIA_APP_ID as string,
+    process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_API_KEY as string
+  );
+
+  const searchClient = {
+    ...algoliaClient,
+    search(queries: readonly MultipleQueriesQuery[]): Promise<any> {
+      if (queries.every(({ params }) => !params?.query)) {
+        return Promise.resolve({
+          results: queries.map(() => ({
+            hits: [],
+            nbHits: 0,
+            nbPages: 0,
+            page: 0,
+            processingTimeMS: 0,
+            hitsPerPage: 0,
+            exhaustiveNbHits: false,
+            query: '',
+            params: '',
+          })),
+        });
+      }
+
+      return algoliaClient.search(queries);
+    },
+  };
+
   const router = useRouter();
   const { posts, tags, test } = props;
 
@@ -76,184 +203,156 @@ const Blog = (props: BlogProps) => {
       }
     >
       <div className="flex h-full w-full flex-col items-center justify-center" />
-      <div className="w-full flex-row sm:flex">
-        <div className="bg-gray-400 px-4 sm:w-1/2 sm:pl-12">
-          <div className="mb-4 sm:mt-20">
-            <h1 className="pt-6 font-helvetica text-3xl font-bold sm:text-[3.5rem]">
-              THINK TANK
-            </h1>
-          </div>
-          <div>
-            <h2 className="font-avenir text-base leading-7 sm:mr-12">
-              Welcome to the Think Tank, my blog where I discuss topics such as
-              startups, coding, travel, fitness, current events, and much more!
-            </h2>
-          </div>
-          <div>
-            <button
-              type="button"
-              disabled
-              className="my-4 py-2 font-raleway text-sm font-semibold hover:decoration-inherit sm:mt-6 sm:h-12"
-            >
-              <Link
-                href="#tags"
-                onClick={(e) => {
-                  e.preventDefault();
-                  const targetId = document.getElementById('tags');
-                  if (targetId) {
-                    targetId.scrollIntoView({
-                      behavior: 'smooth',
-                    });
-                  }
-                }}
-              >
-                Read More
-              </Link>
-            </button>
-          </div>
-        </div>
-        <div className="sm:w-1/2">
-          <Image
-            width={400}
-            height={400}
-            priority
-            src={`${test ? '/' : ''}${
-              router.basePath
-            }/assets/images/thinktank2.png`}
-            alt="Think Tank logo"
-            className="h-[230px] w-full object-cover sm:h-[400px]"
-          />
-        </div>
-      </div>
-      <div
-        id="tags"
-        className="items-center sm:ml-4 sm:flex sm:flex-row sm:overflow-x-scroll"
+      <InstantSearch
+        indexName="post"
+        searchClient={searchClient}
+        future={{
+          preserveSharedStateOnUnmount: true,
+        }}
       >
-        <style>
-          {`
+        <div className="w-full flex-row sm:flex">
+          <div className="bg-gray-400 px-4 sm:w-1/2 sm:pl-12">
+            <div className="mb-4 sm:mt-20">
+              <h1 className="pt-6 font-helvetica text-3xl font-bold sm:text-[3.5rem]">
+                THINK TANK
+              </h1>
+            </div>
+            <div>
+              <h2 className="font-avenir text-base leading-7 sm:mr-12">
+                Welcome to the Think Tank, my blog where I discuss topics such
+                as startups, coding, travel, fitness, current events, and much
+                more!
+              </h2>
+            </div>
+            <div>
+              <button
+                type="button"
+                disabled
+                className="my-4 py-2 font-raleway text-sm font-semibold hover:decoration-inherit sm:mt-6 sm:h-12"
+              >
+                <Link
+                  href="#tags"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    const targetId = document.getElementById('tags');
+                    if (targetId) {
+                      targetId.scrollIntoView({
+                        behavior: 'smooth',
+                      });
+                    }
+                  }}
+                >
+                  Read More
+                </Link>
+              </button>
+            </div>
+          </div>
+          <div className="sm:w-1/2">
+            <Image
+              width={400}
+              height={400}
+              priority
+              src={`${test ? '/' : ''}${
+                router.basePath
+              }/assets/images/thinktank2.png`}
+              alt="Think Tank logo"
+              className="h-[230px] w-full object-cover sm:h-[400px]"
+            />
+          </div>
+        </div>
+        <div
+          id="tags"
+          className="items-center sm:ml-4 sm:flex sm:flex-row sm:overflow-x-scroll"
+        >
+          <style>
+            {`
             @media (min-width: 640px) {
               div#tags {
                 overflow: initial;
               }
             }
           `}
-        </style>
-        <div className="flex flex-row overflow-x-scroll pr-4" id="tags">
-          {[{ id: 'all', name: 'All Posts' }, ...tags].map((tag: any) => (
-            <div key={tag.id} className="mx-4">
-              <Link
-                href={{
-                  pathname:
-                    tag.name === 'All Posts' ? `/blog` : `/blog/${tag.slug}`,
-                }}
-                onClick={(e) => {
-                  e.preventDefault();
-                  router.push(
-                    tag.name === 'All Posts'
-                      ? `/blog#tags`
-                      : `/blog/${tag.slug}#tags`
-                  );
-                }}
-              >
-                {tag.name === 'Current Events' || tag.name === 'All Posts' ? (
-                  <h2 className="my-4 flex w-max text-center font-avenir text-lg sm:my-7 sm:text-center">
-                    {tag.name}
-                  </h2>
-                ) : (
-                  <h2 className="my-4 text-center font-avenir text-lg sm:my-7 sm:text-center">
-                    {tag.name}
-                  </h2>
-                )}
-              </Link>
-            </div>
-          ))}
+          </style>
+          <div className="flex flex-row overflow-x-scroll pr-4" id="tags">
+            {[{ id: 'all', name: 'All Posts' }, ...tags].map((tag: any) => (
+              <div key={tag.id} className="mx-4">
+                <Link
+                  href={{
+                    pathname:
+                      tag.name === 'All Posts' ? `/blog` : `/blog/${tag.slug}`,
+                  }}
+                  onClick={(e) => {
+                    e.preventDefault();
+                    router.push(
+                      tag.name === 'All Posts'
+                        ? `/blog#tags`
+                        : `/blog/${tag.slug}#tags`
+                    );
+                  }}
+                >
+                  {tag.name === 'Current Events' || tag.name === 'All Posts' ? (
+                    <h2 className="my-4 flex w-max text-center font-avenir text-lg sm:my-7 sm:text-center">
+                      {tag.name}
+                    </h2>
+                  ) : (
+                    <h2 className="my-4 text-center font-avenir text-lg sm:my-7 sm:text-center">
+                      {tag.name}
+                    </h2>
+                  )}
+                </Link>
+              </div>
+            ))}
+          </div>
+          <div className="flex w-full flex-row justify-center border py-2 sm:mr-4 sm:w-1/2 sm:justify-end sm:border-0">
+            <SearchBox
+              classNames={{
+                root: 'display-flex flex-row justify-center border-2 border-gray-400 rounded-md outline-none mr-4',
+                input:
+                  'pl-4 pt-2 pb-2 text-sm placeholder-gray-400 outline-none rounded-md',
+                form: 'flex flex-row justify-center rounded-md',
+                submit: 'p-2 text-sm outline-none rounded-md',
+                reset: 'hidden',
+                loadingIndicator: 'hidden',
+              }}
+              placeholder="Search"
+              {...(props as SearchBoxProps)}
+            />
+          </div>
         </div>
 
-        <div className="flex w-full flex-row justify-center border py-2 sm:mr-4 sm:w-1/2 sm:justify-end sm:border-0">
-          <Search onSearch={() => {}} />
-        </div>
-      </div>
-      <div className="-z-10 flex flex-wrap justify-between">
-        <InfiniteScroll
-          dataLength={postList.length}
-          next={() => getAdditionalPosts()}
-          hasMore={hasMore}
-          loader={<h4 style={{ textAlign: 'center' }}>Loading...</h4>}
-          endMessage={
-            <p style={{ textAlign: 'center' }}>
-              <b>Thanks for scrolling! That's all for now!</b>
-            </p>
-          }
-        >
-          {postList.map((post: any, index: number) => {
-            if (index % 2 === 0) {
-              return (
-                <div
-                  className="-z-20 sm:flex sm:flex-row"
-                  key={post.id + new Date().toLocaleDateString()}
-                >
-                  <div className="col-span-1 mb-8 w-fit sm:mx-7" key={post.id}>
-                    <Link
-                      className="w-fit"
-                      href={{
-                        pathname: `/blog/${post.primary_tag.slug}/${post.slug}`,
-                      }}
-                    >
-                      <BlogPreview
-                        excerpt={post.excerpt}
-                        feature_image={post.feature_image}
-                        feature_image_alt={post.feature_image_alt}
-                        primary_author={post.primary_author}
-                        primary_tag={post.primary_tag}
-                        published_at={post.published_at}
-                        reading_time={post.reading_time}
-                        title={post.title}
-                      />
-                    </Link>
-                  </div>
-                  {postList[index + 1] && (
-                    <div
-                      className="col-span-2 mb-8 w-fit sm:mx-7"
-                      key={postList[index + 1].id}
-                    >
-                      <Link
-                        className="w-fit"
-                        href={{
-                          pathname: `/blog/${
-                            postList[index + 1].primary_tag.slug
-                          }/${postList[index + 1].slug}`,
-                        }}
-                      >
-                        <BlogPreview
-                          excerpt={postList[index + 1].excerpt}
-                          feature_image={postList[index + 1].feature_image}
-                          feature_image_alt={
-                            postList[index + 1].feature_image_alt
-                          }
-                          primary_author={postList[index + 1].primary_author}
-                          primary_tag={postList[index + 1].primary_tag}
-                          published_at={postList[index + 1].published_at}
-                          reading_time={postList[index + 1].reading_time}
-                          title={postList[index + 1].title}
-                        />
-                      </Link>
-                    </div>
-                  )}
-                </div>
-              );
+        <div className="-z-10 flex w-full flex-wrap justify-between">
+          <InfiniteScroll
+            dataLength={postList.length}
+            next={() => getAdditionalPosts()}
+            hasMore={hasMore}
+            loader={<h4 style={{ textAlign: 'center' }}>Loading...</h4>}
+            endMessage={
+              <p style={{ textAlign: 'center' }}>
+                <b>Thanks for scrolling! That's all for now!</b>
+              </p>
             }
-            return null;
-          })}
-        </InfiniteScroll>
-        <Subscribe open={openSubscribe} setOpen={setOpenSubscribe} />
-        <div
-          className={`transition-all duration-500 ${
-            openSubscribe ? 'fixed z-50' : 'opacity-0'
-          }`}
-        >
-          <SubscribeModal open={openSubscribe} setOpen={setOpenSubscribe} />
+          >
+            <div className="flex flex-wrap justify-between">
+              <EmptyQueryBoundary postList={postList}>
+                <Hits
+                  hitComponent={Hit}
+                  classNames={{
+                    list: 'flex flex-wrap',
+                  }}
+                />
+              </EmptyQueryBoundary>
+            </div>
+          </InfiniteScroll>
+          <Subscribe open={openSubscribe} setOpen={setOpenSubscribe} />
+          <div
+            className={`transition-all duration-500 ${
+              openSubscribe ? 'fixed z-50' : 'opacity-0'
+            }`}
+          >
+            <SubscribeModal open={openSubscribe} setOpen={setOpenSubscribe} />
+          </div>
         </div>
-      </div>
+      </InstantSearch>
     </Main>
   );
 };
